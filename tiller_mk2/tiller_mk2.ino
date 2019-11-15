c24ba446434f864af45e130a40144ca54b95fc7f
Mike Itz <mike@itzes.com>
2019-11-15 08:55:20 -0500

    new matrix scanning

--- tiller_mk2/tiller_mk2.ino
+++ tiller_mk2/tiller_mk2.ino
@@ -1,80 +1,70 @@
 #include "nrf_gzll.h"
 
-#define num_cols 7
-#define num_rows 3
+#define debug if(Serial) { Serial.println(""); }
 
+#define num_rows 3
+#define num_cols 7
+#define keys (num_rows * num_cols)
+#define matrix uint32_t
+#define bit(r, c) (((matrix)1) << (c + r * num_cols))
+#define set(s, r, c, v) (s |= v ? bit(r, c) : 0)
+#define get(s, r, c) (s & bit(r, c) ? 1 : 0)
 const uint8_t rows[num_rows] = {13, 12, 11};
 const uint8_t cols[num_cols] = {PIN_A0, PIN_A1, PIN_A2, PIN_A3, PIN_A4, PIN_A5, PIN_SPI_SCK};
 
-uint8_t matrix[num_rows + 1];
-uint8_t lastMatrix[num_rows];
-int scanTimeMs = 100;
+matrix state = 0;
+
+#define delayPerTick 2
+#define debounceDownTicks 3
+#define debounceUpTicks 6
+#define sleepAfterIdleTicks (1000/delayPerTick)
+#define repeatTransmitTicks (500/delayPerTick)
+
+int ticksSinceDiff = 0;
+int ticksSinceTransmit = 0;
+uint8_t debounceTicks[keys];
+bool sleeping = false;
 
 ///////////////////////////////////////////////////// MATRIX
 
-inline void clearMatrix(uint8_t *m) {
-  memset(m, 0, num_rows);
-}
 
-inline void copyMatrixTo(uint8_t *other) {
-  memcpy(other, matrix, num_rows);
-}
-
-inline bool different(uint8_t *a, uint8_t *b) {
-  return memcmp(a, b, num_rows);
-}
-
-inline void setKey(uint8_t r, uint8_t c, bool set) {
-  matrix[r] = matrix[r] | (set << c);
-}
-
-inline bool getKey(uint8_t r, uint8_t c) {
-  return (matrix[r] & (1 << c)) != 0;
-}
-
-bool anyKeyDown() {
-  for (int r = 0; r < num_rows; ++r) {
-    if (matrix[r]) {
-      return true;
-    }
-  }
-  return false;
-}
-
-void printMatrix() {
+void printMatrix(matrix state) {
   if (!Serial) {
     return;
   }
   Serial.println("");
   for (uint8_t r = 0; r < num_rows; r++) {
     for (uint8_t c = 0; c < num_cols; c++) {
-      Serial.print(getKey(r, c));
+      Serial.print(get(state, r, c));
     }
     Serial.println("");
   }
   Serial.println("");
 }
 
-void scanMatrix() {
-  clearMatrix(matrix);
-  for (uint8_t c = 0; c < num_cols; ++c) {
-    digitalWrite(cols[c], HIGH);
-    for (uint8_t r = 0; r < num_rows; ++r) {
-      bool bit = digitalRead(rows[r]);
-      setKey(r, c, bit ? 1 : 0);
-    }
-    digitalWrite(cols[c], LOW);
+void initMatrix() {
+  for (int r = 0; r < num_rows; ++r) {
+    pinMode(rows[r], OUTPUT);
+    digitalWrite(rows[r], HIGH);
   }
+  for (int c = 0; c < num_cols; ++c) {
+    pinMode(cols[c], INPUT_PULLUP);
+  }
+  ticksSinceDiff = 0;
+  ticksSinceTransmit = repeatTransmitTicks;
+  state = 0;
 }
 
-void initMatrix() {
-  for (uint8_t c = 0; c < num_cols; ++c) {
-    pinMode(cols[c], OUTPUT);
-    digitalWrite(cols[c], LOW);
+matrix scanMatrix() {
+  matrix scan = 0;
+  for (int r = 0; r < num_rows; ++r) {
+    digitalWrite(rows[r], LOW);
+    for (int c = 0; c < num_cols; ++c) {
+      set(scan, r, c, !digitalRead(cols[c]));
+    }
+    digitalWrite(rows[r], HIGH);
   }
-  for (uint8_t r = 0; r < num_rows; ++r) {
-    pinMode(rows[r], INPUT_PULLDOWN);
-  }
+  return scan;
 }
 
 void setup() {
@@ -85,9 +75,8 @@ void setup() {
 }
 
 void loop() {
-  scanMatrix();
-  printMatrix();
-  delay(scanTimeMs);
+  printMatrix(scanMatrix());
+  delay(500);
 }
 
 void nrf_gzll_device_tx_success(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {}
