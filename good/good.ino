15adc59dcd4175575c49f1df9c49a2bc65c065df
Mike Itz <mike@itzes.com>
2019-12-16 23:52:23 -0500

    checkpoint known good firmware to test whether software or hardware issue

--- good/good.ino
+++ good/good.ino
@@ -1,79 +1,81 @@
 #include "nrf.h"
 #include "nrf_gzll.h"
 
-#define PIPE 3
-#define DEBUG 0
+#define debug 0
 
-#define ROWS 3
-#define COLS 7
-#define KEYS (ROWS * COLS)
-#define matrix_t uint32_t
-#define bit(r, c) (((matrix_t)1) << (c + r * COLS))
+#define num_rows 3
+#define num_cols 7
+#define keys (num_rows * num_cols)
+#define matrix uint32_t
+#define bit(r, c) (((matrix)1) << (c + r * num_cols))
 #define set(s, r, c, v) (s |= v ? bit(r, c) : 0)
 #define get(s, r, c) (s & bit(r, c) ? 1 : 0)
+const uint8_t rows[num_rows] = {13, 12, 11};
+const uint8_t cols_slim[num_cols] = {PIN_A0, PIN_A1, PIN_A2, PIN_A3, PIN_A4, PIN_A5, PIN_SPI_SCK};
+const uint8_t cols_thick[num_cols] = {PIN_SPI_SCK, PIN_A5, PIN_A4, PIN_A3, PIN_A2, PIN_A1, PIN_A0};
+const uint8_t* cols = nullptr;
 
-const uint8_t rows[ROWS] = {13, 12, 11};
-#if (PIPE == 3 || PIPE == 4)
-  const uint8_t cols[COLS] = {PIN_SPI_SCK, PIN_A5, PIN_A4, PIN_A3, PIN_A2, PIN_A1, PIN_A0};
-#else
-  const uint8_t cols[COLS] = {PIN_A0, PIN_A1, PIN_A2, PIN_A3, PIN_A4, PIN_A5, PIN_SPI_SCK};
-#endif
+matrix state = 0;
+int pipe = 3;
 
-#define packet_t_size 4
-matrix_t state = 0;
-matrix_t lastScan;
-
-#define delayPerTick 1
-#define numDebounce 6
-#define sleepAfterIdleTicks (1500/delayPerTick)
-#define repeatTransmitTicks (400/delayPerTick)
+#define delayPerTick 2
+#define debounceDownTicks 3
+#define debounceUpTicks 3
+#define sleepAfterIdleTicks (1000/delayPerTick)
+#define repeatTransmitTicks (500/delayPerTick)
 
 int ticksSinceDiff = 0;
 int ticksSinceTransmit = 0;
-int debounceCount = 0;
-volatile bool sleeping = false;
-volatile bool waking = true;
+uint8_t debounceTicks[keys];
+bool sleeping = false;
+bool waking = true;
 
 uint8_t ack_payload[NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH];
 uint32_t ack_payload_length = 0;
 uint8_t data_buffer[NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH];
-#if (PIPE % 2)
-  uint8_t channel_table[3] = {25, 63, 33};
-#else
-  uint8_t channel_table[3] = {4, 42, 77};
-#endif
+uint8_t channel_table[3] = {4, 42, 77};
 
 ///////////////////////////////////////////////////// MATRIX
 
-void printMatrix(matrix_t state) {
-  for (uint8_t r = 0; r < ROWS; r++) {
-    for (uint8_t c = 0; c < COLS; c++) {
+
+void printMatrix(matrix state) {
+  if (!Serial) {
+    return;
+  }
+  Serial.println("");
+  for (uint8_t r = 0; r < num_rows; r++) {
+    for (uint8_t c = 0; c < num_cols; c++) {
       Serial.print(get(state, r, c));
     }
-    Serial.print(" ");
+    Serial.println("");
   }
+  Serial.println("");
 }
 
 void initMatrix() {
-  for (int r = 0; r < ROWS; ++r) {
+  if (pipe == 3 || pipe == 4) {
+    cols = cols_thick;
+  } else {
+    cols = cols_slim;
+  }
+  for (int r = 0; r < num_rows; ++r) {
     pinMode(rows[r], OUTPUT);
     digitalWrite(rows[r], HIGH);
   }
-  for (int c = 0; c < COLS; ++c) {
+  for (int c = 0; c < num_cols; ++c) {
     pinMode(cols[c], INPUT_PULLUP);
   }
   ticksSinceDiff = 0;
   ticksSinceTransmit = repeatTransmitTicks;
   state = 0;
-  lastScan = 0;
-  debounceCount = 0;
+  memset(debounceTicks, 0, sizeof(debounceTicks));
 }
 
-matrix_t scanMatrix() {
-  matrix_t scan = 0;
-  for (int r = 0; r < ROWS; ++r) {
+matrix scanMatrix() {
+  matrix scan = 0;
+  for (int r = 0; r < num_rows; ++r) {
     digitalWrite(rows[r], LOW);
-    for (int c = 0; c < COLS; ++c) {
+    for (int c = 0; c < num_cols; ++c) {
       set(scan, r, c, !digitalRead(cols[c]));
     }
     digitalWrite(rows[r], HIGH);
@@ -82,26 +84,26 @@ matrix_t scanMatrix() {
 }
 
 bool scanWithDebounce() {
-  matrix_t scan = scanMatrix();
-  if (scan == state || scan != lastScan) {
-    debounceCount = 0;
-    lastScan = scan;
-    return false;
-  } else if (debounceCount > numDebounce) {
-    debounceCount = 0;
-    state = scan;
-    return true;
-  } else {
-    debounceCount++;
-    return false;
+  matrix scan = scanMatrix(); 
+  bool diff = false;
+  for (int i = 0; i < keys; ++i) {
+    matrix b = ((matrix)1) << i;
+    if (debounceTicks[i] > 0) {
+      debounceTicks[i]--;
+    } else if ((scan & b) != (state & b)) {
+      state = (state & ~b) | (scan & b);
+      debounceTicks[i] = (scan & b) ? debounceDownTicks : debounceUpTicks;
+      diff = true;
+    }
   }
+  return diff;
 }
 
 ///////////////////////////////////////////////////// POWER
 
 inline void pinModeDetect(uint32_t pin) {
   pin = g_ADigitalPinMap[pin];
-  NRF_GPIO_Type *port = nrf_gpio_pin_port_decode(&pin);
+  NRF_GPIO_Type * port = nrf_gpio_pin_port_decode(&pin);
   port->PIN_CNF[pin] = ((uint32_t)GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos)
                        | ((uint32_t)GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos)
                        | ((uint32_t)GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos)
@@ -112,23 +114,35 @@ inline void pinModeDetect(uint32_t pin) {
 void sleep() {
   if (sleeping) return;
   sleeping = true;
-  for (int r = 0; r < ROWS; ++r) {
+  for (int r = 0; r < num_rows; ++r) {
     digitalWrite(rows[r], LOW);
   }
   NRF_GPIOTE->EVENTS_PORT = 0;
   NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_PORT_Msk;
-  for (int c = 0; c < COLS; ++c) {
+  for (int c = 0; c < num_cols; ++c) {
     pinModeDetect(cols[c]);
   }
   suspendLoop();
 }
 
 void wake() {
+  if (!sleeping) return;
   sleeping = false;
   waking = true;
   NRF_GPIOTE->EVENTS_PORT = 0;
   NRF_GPIOTE->INTENCLR |= GPIOTE_INTENSET_PORT_Msk;
+  if (Serial && !debug) {
+    Serial.end();
+  }
+  initMatrix();
   resumeLoop();
+}
+
+void killSerial() {
+  NRF_UART0->TASKS_STOPTX = 1;
+  NRF_UART0->TASKS_STOPRX = 1;
+  NRF_UART0->ENABLE = 0;
+  NRF_SPI0->ENABLE = 0;
 }
 
 void initCore() {
@@ -150,27 +164,19 @@ void initRadio() {
   nrf_gzll_set_timeslot_period(900);
   nrf_gzll_set_base_address_0(0x01020304);
   nrf_gzll_set_base_address_1(0x05060708);
-  nrf_gzll_set_tx_power(NRF_GZLL_TX_POWER_4_DBM);
   nrf_gzll_enable();
 }
 
 void transmit() {
-  nrf_gzll_add_packet_to_tx_fifo(PIPE, (uint8_t*)&state, 4);
-  #if DEBUG
-    static matrix_t printed;
-    if (printed != state) {
-      Serial.println("");
-      printMatrix(state);
-      printed = state;
-    } else {
-      Serial.println(".");
-    }
-  #endif
+  // printMatrix(state);
+  ticksSinceTransmit = 0;
+  nrf_gzll_add_packet_to_tx_fifo(pipe, (uint8_t*)&state, 4);
 }
 
 void nrf_gzll_device_tx_success(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {
   uint32_t ack_payload_length = NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH;
-  if (tx_info.payload_received_in_ack) {
+  if (tx_info.payload_received_in_ack)
+  {
     nrf_gzll_fetch_packet_from_rx_fifo(pipe, ack_payload, &ack_payload_length);
   }
 }
@@ -182,45 +188,27 @@ void nrf_gzll_host_rx_data_ready(uint32_t pipe, nrf_gzll_host_rx_info_t rx_info)
 ///////////////////////////////////////////////////// MAIN
 
 void setup() {
-  #if DEBUG
+  if (debug) {
     Serial.begin(115200);
-  #endif
+  }
   initCore();
+  initMatrix();
   initRadio();
 }
 
 void loop() {
-  if (sleeping) {
-    delay(1);
-    return;
-  }
   if (waking) {
-    #if DEBUG
-      Serial.println("wake");
-      Serial.flush();
-    #else
-      if (Serial) Serial.end();
-    #endif
     waking = false;
-    initMatrix();
-  }
-  if (scanWithDebounce()) {
-    transmit();
-    ticksSinceTransmit = ticksSinceDiff = 0;
-  } else if (ticksSinceDiff > sleepAfterIdleTicks && state == 0) {
-    #if DEBUG
-      Serial.println("sleep");
-      Serial.flush();
-    #endif
-    sleep();
-    return;
-  } else if (ticksSinceTransmit > repeatTransmitTicks) {
-    transmit();
-    ticksSinceDiff++;
-    ticksSinceTransmit = 0;
-  } else {
-    ticksSinceDiff++;
-    ticksSinceTransmit++;
   }
   delay(delayPerTick);
+  ticksSinceDiff++;
+  ticksSinceTransmit++;
+  if (scanWithDebounce()) {
+    transmit();
+    ticksSinceDiff = 0;
+  } else if (ticksSinceDiff > sleepAfterIdleTicks && state == 0) {
+    sleep();
+  } else if (ticksSinceTransmit > repeatTransmitTicks) {
+    transmit();
+  }
 }
