db43373e62959f95fae51b4fa2657fb0375e673d
mike@itzes.com <Mike Itz>
2019-12-21 13:34:28 -0500

    port keybed to the nrf52

--- keybed/keybed.ino
+++ keybed/keybed.ino
@@ -1,12 +1,19 @@
-#include "MIDIUSB.h"
+#include "nrf.h"
+
+/////// MATRIX
 
 #define VMIN 3000
 #define VMAX 50000
 
-const uint8_t rowScan[8] = {2, 4, 8, 16, 32, 64, 128, 1};
-
 uint8_t last1[8], last2[8];
 unsigned long time1[64], time2[64];
+
+void initMatrix() {
+  memset(last1, 255, 8);
+  memset(last2, 255, 8);
+  memset(time1, 0, 64 * 4);
+  memset(time2, 0, 64 * 4);
+}
 
 void show(uint8_t b) {
   for (int i = 0; i < 8; ++i) {
@@ -27,25 +34,21 @@ uint8_t vel(long t) {
 void noteOn(uint8_t i, uint8_t j, unsigned long t) {
   uint8_t n = note(i, j);
   uint8_t v = vel(t);
-  MidiUSB.sendMIDI({0x09, 0x90, n, v});
-  MidiUSB.flush();
-  /*Serial.print("ON ");
+  Serial.print("ON ");
   Serial.print(n);
   Serial.print(" ");
   Serial.println(vel(t));
-  Serial.flush();*/
+  Serial.flush();
 }
 
 void noteOff(uint8_t i, uint8_t j, unsigned long t) {
   uint8_t n = note(i, j);
   uint8_t v = vel(t);
-  MidiUSB.sendMIDI({0x08, 0x80, n, v});
-  MidiUSB.flush();
-  /*Serial.print("OFF ");
+  Serial.print("OFF ");
   Serial.print(n);
   Serial.print(" ");
   Serial.println(vel(t));
-  Serial.flush();*/
+  Serial.flush();
 }
 
 void updateScan(unsigned long t, uint8_t i, uint8_t scan1, uint8_t scan2) {
@@ -83,28 +86,57 @@ void updateScan(unsigned long t, uint8_t i, uint8_t scan1, uint8_t scan2) {
   }
 }
 
-void setup() {
-  DDRD = 255;
-  PORTD = 254;
-  
-  DDRC = 0;
-  PORTC = 255;
+/////// CORE IO
 
-  DDRB = 0;
-  PORTB = 255;
-  
+const uint8_t rows[8] = {2, 3, 4, 5, 28, 29, 30, 31};
+uint32_t rowScan[8];
+uint32_t rowMask = 0;
+
+void nfcAsGpio() {
+  if ((NRF_UICR->NFCPINS & UICR_NFCPINS_PROTECT_Msk) == (UICR_NFCPINS_PROTECT_NFC << UICR_NFCPINS_PROTECT_Pos)){
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+    NRF_UICR->NFCPINS &= ~UICR_NFCPINS_PROTECT_Msk;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+    NVIC_SystemReset();
+  }
+}
+
+void setup() {
+  nfcAsGpio();
   Serial.begin(115200);
-  memset(last1, 255, 8);
-  memset(last2, 255, 8);
-  memset(time1, 0, 64 * 4);
-  memset(time2, 0, 64 * 4);
+  for (int i = 9; i <= 23; ++i) {
+    if (i == 21) continue;
+    pinMode(i, INPUT_PULLUP);
+  }
+  for (int i = 0; i <= 8; ++i) {
+    pinMode(rows[i], OUTPUT);
+    digitalWrite(rows[i], HIGH);
+    rowMask |= (rowScan[i] = (1UL << rows[i]));
+  }
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+  initMatrix();
+}
+
+inline uint8_t scanUpper(uint32_t s) {
+  // 16, 17, 18, 19, 20, 22, 23
+  return ((s >> 16) & 0x1fUL) | ((s >> 17) & 0x60UL);
+}
+
+inline uint8_t scanLower(uint32_t s) {
+  // 9, 10, 11, 12, 13, 14, 15
+  return (s >> 9) & 0x7fUL;
 }
 
 void loop() {
   for(uint8_t i = 0; i < 8; ++i) {
+    NRF_GPIO->OUTSET = rowMask;
+    NRF_GPIO->OUTCLR = rowScan[i];
     unsigned long t = micros();
-    uint8_t scan1 = PINB, scan2 = PINC;
-    PORTD = ~rowScan[i];
-    updateScan(t, i, scan1, scan2);
+    uint32_t scan = NRF_GPIO->IN;
+    updateScan(t, i, scanUpper(scan), scanLower(scan));
   }
 }
