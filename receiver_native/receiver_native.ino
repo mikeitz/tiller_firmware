29eebdf3118c86c1105986417e1b6761da2a5b64
Mike Itz <mike@itzes.com>
2022-04-06 16:31:39 -0400

    structure code and data into classes

--- receiver_native/receiver_native.ino
+++ receiver_native/receiver_native.ino
@@ -1,234 +1,233 @@
 #include "nrf_gzll.h"
 #include <Adafruit_TinyUSB.h>
 
-///////////////////////////////////////// INTEROP
-
-const uint32_t queue_size = 4096;
-volatile uint8_t queue_buffer[queue_size];
-volatile uint32_t read_count = 0;
-volatile uint32_t write_count = 0;
-
-void enqueueMessage(uint8_t pipe, uint8_t length, uint8_t* data) {
-  uint32_t cursor = write_count;
-  queue_buffer[(cursor++) % queue_size] = pipe;
-  queue_buffer[(cursor++) % queue_size] = length;
-  for (uint8_t i = 0; i < length; ++i) {
-    queue_buffer[(cursor++) % queue_size] = data[i];
-  }
-  write_count = cursor;
-}
-
-bool hasMessage() {
-  return write_count > read_count;
-}
-
-bool dequeueMessage(uint8_t* pipe, uint8_t* length, uint8_t* data) {
-  if (!hasMessage()) {
-    *pipe = -1;
-    *length = 0;
-    return false;
-  }
-  uint32_t cursor = read_count;
-  *pipe = queue_buffer[(cursor++) % queue_size];
-  *length = queue_buffer[(cursor++) % queue_size];
-  for (uint8_t i = 0; i < *length; ++i) {
-    data[i] = queue_buffer[(cursor++) % queue_size];
-  }
-  read_count = cursor;
-  return true;
-}
+const int num_pipes = 8;
+const int num_keys_per_pipe = 32;
+const int num_layers = 16;
 
 ///////////////////////////////////////// RADIO
 
-void initRadio() {
-  static uint8_t channel_table[6] = { 4, 25, 42, 63, 77, 33 };
-  nrf_gzll_init(NRF_GZLL_MODE_HOST);
-  nrf_gzll_set_channel_table(channel_table, 3);
-  nrf_gzll_set_timeslots_per_channel(4);
-  nrf_gzll_set_datarate(NRF_GZLL_DATARATE_1MBIT);
-  nrf_gzll_set_timeslot_period(900);
-  nrf_gzll_set_base_address_0(0x01020304);
-  nrf_gzll_set_base_address_1(0x05060708);
-  nrf_gzll_set_tx_power(NRF_GZLL_TX_POWER_N8_DBM);
-  nrf_gzll_enable();
-}
-
-void nrf_gzll_device_tx_success(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {}
-void nrf_gzll_device_tx_failed(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {}
-void nrf_gzll_disabled() {}
-void nrf_gzll_host_rx_data_ready(uint32_t pipe, nrf_gzll_host_rx_info_t rx_info) {
-  uint32_t length = NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH;
-  static uint8_t new_data[NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH];
-  if (nrf_gzll_fetch_packet_from_rx_fifo(pipe, new_data, &length)) {
-    enqueueMessage(pipe, length, new_data);
+class Radio {
+public:
+  void Init() {
+    static uint8_t channel_table[6] = { 4, 25, 42, 63, 77, 33 };
+    nrf_gzll_init(NRF_GZLL_MODE_HOST);
+    nrf_gzll_set_channel_table(channel_table, 3);
+    nrf_gzll_set_timeslots_per_channel(4);
+    nrf_gzll_set_datarate(NRF_GZLL_DATARATE_1MBIT);
+    nrf_gzll_set_timeslot_period(900);
+    nrf_gzll_set_base_address_0(0x01020304);
+    nrf_gzll_set_base_address_1(0x05060708);
+    nrf_gzll_set_tx_power(NRF_GZLL_TX_POWER_N8_DBM);
+    nrf_gzll_enable();
   }
-}
+  void EnqueueMessage(uint8_t pipe, uint8_t length, uint8_t* data) {
+    uint32_t cursor = write_count_;
+    queue_buffer_[(cursor++) % queue_size_] = pipe;
+    queue_buffer_[(cursor++) % queue_size_] = length;
+    for (uint8_t i = 0; i < length; ++i) {
+      queue_buffer_[(cursor++) % queue_size_] = data[i];
+    }
+    write_count_ = cursor;
+  }
+  bool HasMessage() {
+    return write_count_ > read_count_;
+  }
+  bool DequeueMessage(uint8_t* pipe, uint8_t* length, uint8_t* data) {
+    if (!HasMessage()) {
+      *pipe = -1;
+      *length = 0;
+      return false;
+    }
+    uint32_t cursor = read_count_;
+    *pipe = queue_buffer_[(cursor++) % queue_size_];
+    *length = queue_buffer_[(cursor++) % queue_size_];
+    for (uint8_t i = 0; i < *length; ++i) {
+      data[i] = queue_buffer_[(cursor++) % queue_size_];
+    }
+    read_count_ = cursor;
+    return true;
+  }
+private:
+  static const uint32_t queue_size_ = 4096;
+  volatile uint8_t queue_buffer_[queue_size_];
+  volatile uint32_t read_count_ = 0;
+  volatile uint32_t write_count_ = 0;
+};
 
 ///////////////////////////////////////// HID
 
-uint8_t const desc_hid_report[] = { TUD_HID_REPORT_DESC_KEYBOARD() };
+static uint8_t const desc_hid_report[] = { TUD_HID_REPORT_DESC_KEYBOARD() };
 Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD, 2, false);
-void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {}
 
-uint32_t reports_generated = 0, reports_sent = 0;
-const int report_buffer_size = 256;
-const int report_size = 7;
-uint8_t report_buffer[report_buffer_size * report_size];
-int8_t mods = 0, per_key_mods = 0;
-uint8_t report[6] = { 0, 0, 0, 0, 0, 0 };
-
-inline bool isMod(uint32_t keycode) {
-  return keycode >= 0xE0 && keycode < 0xF0;
-}
-
-inline void setMod(uint32_t keycode) {
-  mods |= 1 << (keycode & 0xf);
-}
-
-inline void clearMod(uint32_t keycode) {
-  mods &= ~(1 << (keycode & 0xf));
-}
-
-inline bool isModSet(uint32_t keycode) {
-  return (1 << (keycode & 0xf)) & mods;
-}
-
-void generateReport() {
-  uint8_t* report_ptr = &report_buffer[(reports_generated * report_size) % report_buffer_size];
-  uint8_t force_on = per_key_mods & 0xf;
-  uint8_t force_off = ((per_key_mods & 0xf0) >> 4) | (per_key_mods & 0xf0);
-  report_ptr[0] = (mods | force_on) & (~force_off);
-  memcpy(report_ptr + 1, report, 6);
-  ++reports_generated;
-}
-
-bool sendReports() {
-  while (reports_sent < reports_generated) {
-    if (!usb_hid.ready()) {
-      return false;
-    }
-    uint8_t* report = &report_buffer[(reports_sent * report_size) % report_buffer_size];
-    usb_hid.keyboardReport(0, report[0], report + 1);
-    ++reports_sent;
-  }
-  return true;
-}
-
-void addToReport(uint8_t keycode) {
-  clearFromReport(keycode);
-  for (int i = 0; i < 6; ++i) {
-    if (report[i] == 0) {
-      report[i] = keycode;
-      break;
+class Hid {
+public:
+  void Init() {
+    usb_hid.setReportCallback(NULL, ReportCallback);
+    usb_hid.begin();
+    while (!TinyUSBDevice.mounted()) {
+      delay(1);
     }
   }
-}
-
-void clearFromReport(uint8_t keycode) {
-  for (int i = 0; i < 6; ++i) {
-    if (report[i] == keycode) {
-      report[i] = 0;
+  inline bool IsMod(uint32_t keycode) {
+    return keycode >= 0xE0 && keycode < 0xF0;
+  }
+  inline void SetMod(uint32_t keycode) {
+    mods_ |= 1 << (keycode & 0xf);
+  }
+  inline void ClearMod(uint32_t keycode) {
+    mods_ &= ~(1 << (keycode & 0xf));
+  }
+  inline bool IsModSet(uint32_t keycode) {
+    return (1 << (keycode & 0xf)) & mods_;
+  }
+  void GenerateReport() {
+    uint8_t* report_ptr = &report_buffer_[(reports_generated_ * report_size_) % report_buffer_size_];
+    uint8_t force_on = per_key_mods_ & 0xf;
+    uint8_t force_off = ((per_key_mods_ & 0xf0) >> 4) | (per_key_mods_ & 0xf0);
+    report_ptr[0] = (mods_ | force_on) & (~force_off);
+    memcpy(report_ptr + 1, report_, 6);
+    ++reports_generated_;
+  }
+  bool SendReports() {
+    while (reports_sent_ < reports_generated_) {
+      if (!usb_hid.ready()) {
+        return false;
+      }
+      uint8_t* report_ = &report_buffer_[(reports_sent_ * report_size_) % report_buffer_size_];
+      usb_hid.keyboardReport(0, report_[0], report_ + 1);
+      ++reports_sent_;
+    }
+    return true;
+  }
+  void AddToReport(uint8_t keycode) {
+    ClearFromReport(keycode);
+    for (int i = 0; i < 6; ++i) {
+      if (report_[i] == 0) {
+        report_[i] = keycode;
+        break;
+      }
     }
   }
-}
-
-void initHid() {
-  usb_hid.setReportCallback(NULL, hid_report_callback);
-  usb_hid.begin();
-  while (!TinyUSBDevice.mounted()) {
-    delay(1);
+  void ClearFromReport(uint8_t keycode) {
+    for (int i = 0; i < 6; ++i) {
+      if (report_[i] == keycode) {
+        report_[i] = 0;
+      }
+    }
   }
-}
+  void SetPerKeyMods(uint8_t per_key_mods) {
+    per_key_mods_ = per_key_mods;
+  }
+  void ResetPerKeyMods() {
+    if (per_key_mods_) {
+      per_key_mods_ = 0;
+      GenerateReport();
+    }
+  }
+private:
+  static const uint32_t report_buffer_size_ = 256;
+  static const uint8_t report_size_ = 7;
+  static void ReportCallback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {}
+  uint32_t reports_generated_ = 0, reports_sent_ = 0;
+  uint8_t report_buffer_[report_buffer_size_ * report_size_];
+  int8_t mods_ = 0, per_key_mods_ = 0;
+  uint8_t report_[6] = { 0, 0, 0, 0, 0, 0 };
+};
 
 ///////////////////////////////////////// LAYERS
 
-const int num_layers = 16;
-int8_t active_layers[num_layers] = { 0 };
-uint32_t active_layer_mask = 1;
-
-void activateLayer(uint8_t layer) {
-  if (isLayerActive(layer)) {
-    return;
+class Layers {
+public:
+  void ActivateLayer(uint8_t layer) {
+    if (IsLayerActive(layer)) {
+      return;
+    }
+    ++num_active_layers_;
+    active_layer_mask_ |= (1 << layer);
+    int i = 0;
+    int8_t move = 0;
+    for (; i < num_layers && active_layers_[i] > layer; ++i) {}
+    move = active_layers_[i];
+    active_layers_[i++] = layer;
+    for (; i < num_layers && move != 0; ++i) {
+      int8_t temp = active_layers_[i];
+      active_layers_[i] = move;
+      move = temp;
+    }
   }
-  active_layer_mask |= (1 << layer);
-  int i = 0;
-  int8_t move = 0;
-  for (; i < num_layers && active_layers[i] > layer; ++i) {}
-  move = active_layers[i];
-  active_layers[i++] = layer;
-  for (; i < num_layers && move != 0; ++i) {
-    int8_t temp = active_layers[i];
-    active_layers[i] = move;
-    move = temp;
+  void DeactivateLayer(uint8_t layer) {
+    if (!IsLayerActive(layer) || layer == 0) {
+      return;
+    }
+    --num_active_layers_;
+    active_layer_mask_ &= ~(1 << layer);
+    int i = 0;
+    for (; i < num_layers && active_layers_[i] != layer; ++i) {}
+    for (; i < num_layers && active_layers_[i] != 0; ++i) {
+      active_layers_[i] = active_layers_[i + 1];
+    }
   }
-}
-
-void deactivateLayer(uint8_t layer) {
-  if (!isLayerActive(layer) || layer == 0) {
-    return;
+  bool IsLayerActive(uint8_t layer) {
+    return (1 << layer) & active_layer_mask_;
   }
-  active_layer_mask &= ~(1 << layer);
-  int i = 0;
-  for (; i < num_layers && active_layers[i] != layer; ++i) {}
-  for (; i < num_layers && active_layers[i] != 0; ++i) {
-    active_layers[i] = active_layers[i + 1];
+  void ToggleLayer(uint8_t layer) {
+    if (IsLayerActive(layer)) {
+      DeactivateLayer(layer);
+    } else {
+      ActivateLayer(layer);
+    }
   }
-}
-
-bool isLayerActive(uint8_t layer) {
-  return (1 << layer) & active_layer_mask;
-}
-
-void toggleLayer(uint8_t layer) {
-  if (isLayerActive(layer)) {
-    deactivateLayer(layer);
-  } else {
-    activateLayer(layer);
+  uint8_t GetNumActiveLayers() {
+    return num_active_layers_;
   }
-}
+  uint8_t GetActiveLayer(uint8_t i) {
+    return active_layers_[i];
+  }
+private:
+  int8_t active_layers_[num_layers] = { 0 };
+  uint32_t active_layer_mask_ = 1;
+  uint8_t num_active_layers_ = 1;
+};
 
-///////////////////////////////////////// KEYMAP
+///////////////////////////////////////// KEYBOARD
 
-const int num_pipes = 8;
-const int num_keys_per_pipe = 32;
-
+Hid hid;
+Layers layers;
 #include "./keymap.h"
 
 uint32_t getKeyFromMap(uint8_t pipe, uint8_t key) {
-  for (int i = 0; i < num_layers; ++i) {
-    uint8_t layer = active_layers[i];
-    uint32_t keycode = keymap[pipe][layer][key];
+  for (int i = 0; i < layers.GetNumActiveLayers(); ++i) {
+    uint32_t keycode = keymap[pipe][layers.GetActiveLayer(i)][key];
     if (keycode == XXX) {
       return 0;
     }
-    if (keycode != ___ || layer == 0) {
+    if (keycode != ___) {
       return keycode;
     }
   }
   return 0;
 }
 
-///////////////////////////////////////// KEYBOARD
-
 uint32_t registerCustom(uint32_t keycode) {
   switch (keycode) {
   case TAB_OR_F4:
-    if (active_layer_mask == 1 && isModSet(HID_KEY_ALT_LEFT)) {
+    if (layers.GetNumActiveLayers() == 1 && hid.IsModSet(HID_KEY_ALT_LEFT)) {
       return registerKey(HID_KEY_F4);
     } else {
       return registerKey(HID_KEY_TAB);
     }
   case GUI_OR_STAB:
-    if (active_layer_mask == 1 && (isModSet(HID_KEY_ALT_LEFT) || isModSet(HID_KEY_CONTROL_LEFT))) {
-      return registerKey(S(HID_KEY_TAB));
+    if (layers.GetNumActiveLayers() == 1 && (hid.IsModSet(HID_KEY_ALT_LEFT) || hid.IsModSet(HID_KEY_CONTROL_LEFT))) {
+      return registerKey(SHIFT(HID_KEY_TAB));
     } else {
       return registerKey(HID_KEY_GUI_LEFT);
     }
   case NUM_OR_TAB:
-    if (active_layer_mask == 1 && (isModSet(HID_KEY_ALT_LEFT) || isModSet(HID_KEY_CONTROL_LEFT))) {
+    if (layers.GetNumActiveLayers() == 1 && (hid.IsModSet(HID_KEY_ALT_LEFT) || hid.IsModSet(HID_KEY_CONTROL_LEFT))) {
       return registerKey(HID_KEY_TAB);
     } else {
-      return registerKey(MO(LAYER_NUM));
+      return registerKey(MOMENTARY(LAYER_NUM));
     }
   default:
     return keycode;
@@ -249,23 +248,23 @@ uint32_t registerKey(uint32_t keycode) {
   if (keycode >= CUSTOM_KEYCODE(0)) {
     return registerCustom(keycode);
   }
-  if (keycode & MO(0xf)) {
+  if (keycode & MOMENTARY(0xf)) {
     uint8_t layer = (keycode >> 16) & 0xf;
-    if (keycode & TG(0)) {
-      toggleLayer(layer);
+    if (keycode & TOGGLE(0)) {
+      layers.ToggleLayer(layer);
     } else {
-      activateLayer(layer);
+      layers.ActivateLayer(layer);
     }
   }
   if (keycode & 0xff00) {
-    per_key_mods = (keycode & 0xff00) >> 8;
+    hid.SetPerKeyMods((keycode & 0xff00) >> 8);
   }
-  if (isMod(keycode)) {
-    setMod(keycode);
-    generateReport();
+  if (hid.IsMod(keycode)) {
+    hid.SetMod(keycode);
+    hid.GenerateReport();
   } else if (keycode & 0xff) {
-    addToReport(keycode & 0xff);
-    generateReport();
+    hid.AddToReport(keycode & 0xff);
+    hid.GenerateReport();
   }
   return keycode;
 }
@@ -277,20 +276,20 @@ void unregisterKey(uint32_t keycode) {
   if (keycode >= CUSTOM_KEYCODE(0)) {
     return unregisterCustom(keycode);
   }
-  if (keycode & MO(0xf)) {
+  if (keycode & MOMENTARY(0xf)) {
     uint8_t layer = (keycode >> 16) & 0xf;
-    if (keycode & TG(0)) {
+    if (keycode & TOGGLE(0)) {
       // No-op.  Toggle only on register.
     } else {
-      deactivateLayer((keycode >> 16) & 0xf);
+      layers.DeactivateLayer((keycode >> 16) & 0xf);
     }
   }
-  if (isMod(keycode)) {
-    clearMod(keycode);
-    generateReport();
+  if (hid.IsMod(keycode)) {
+    hid.ClearMod(keycode);
+    hid.GenerateReport();
   } else if (keycode & 0xff) {
-    clearFromReport(keycode & 0xff);
-    generateReport();
+    hid.ClearFromReport(keycode & 0xff);
+    hid.GenerateReport();
   }
 }
 
@@ -304,7 +303,7 @@ void updatePipe(uint8_t pipe, uint32_t new_state) {
     for (int i = 0; i < num_keys_per_pipe; ++i) {
       uint32_t bit = 1ul << i;
       if ((old_state & bit) != (new_state & bit)) {
-        per_key_mods = 0;
+        hid.SetPerKeyMods(0);
         if (new_state & bit) {
           release_keymap[pipe][i] = registerKey(getKeyFromMap(pipe, i));
         } else {
@@ -313,34 +312,44 @@ void updatePipe(uint8_t pipe, uint32_t new_state) {
       }
     }
   }
-  if (per_key_mods) {
-    per_key_mods = 0;
-    generateReport();
-  }
+  hid.ResetPerKeyMods();
   pipe_state[pipe] = new_state;
 }
 
 ///////////////////////////////////////// MAIN
 
+Radio radio;
+
+void nrf_gzll_device_tx_success(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {}
+void nrf_gzll_device_tx_failed(uint32_t pipe, nrf_gzll_device_tx_info_t tx_info) {}
+void nrf_gzll_disabled() {}
+void nrf_gzll_host_rx_data_ready(uint32_t pipe, nrf_gzll_host_rx_info_t rx_info) {
+  uint32_t length = NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH;
+  static uint8_t new_data[NRF_GZLL_CONST_MAX_PAYLOAD_LENGTH];
+  if (nrf_gzll_fetch_packet_from_rx_fifo(pipe, new_data, &length)) {
+    radio.EnqueueMessage(pipe, length, new_data);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
-  initHid();
-  initRadio();
+  hid.Init();
+  radio.Init();
   delay(1000);
 }
 
 void loop() {
   delay(1);
 
-  if (hasMessage() && TinyUSBDevice.suspended()) {
+  if (radio.HasMessage() && TinyUSBDevice.suspended()) {
     TinyUSBDevice.remoteWakeup();
   }
 
-  sendReports();
+  hid.SendReports();
 
   static uint8_t pipe, length;
   static uint8_t data[256];
-  while (dequeueMessage(&pipe, &length, data)) {
+  while (radio.DequeueMessage(&pipe, &length, data)) {
     if (pipe < 5 && length == 4) {
       updatePipe(pipe, *(uint32_t*)data);
     }
